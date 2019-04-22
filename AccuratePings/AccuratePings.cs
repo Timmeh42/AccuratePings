1509b65588087465c1727c82e3a0db33398b1c72
Tim <tmeier42+github@gmail.com>
2019-04-22 21:23:33 +0200

    first version of AccuratePings

---
+++ AccuratePings/AccuratePings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using BepInEx;
+using MonoMod.Cil;
+
+namespace AccuratePings
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.Timmeh42.AccuratePings", "Accurate Pings", "1.0.0")]
+    public class AccuratePings : BaseUnityPlugin
+    {
+        public float ParseFloat(string strInput, float defaultVal = 1.0f, float min = float.MinValue, float max = float.MaxValue)
+        {
+            if (float.TryParse(strInput, NumberStyles.Any, CultureInfo.InvariantCulture, out float parsedFloat))
+            {
+                return parsedFloat <= min ? min : parsedFloat >= max ? max : parsedFloat;
+            }
+            return defaultVal;
+        }
+
+        float coneAngle => ParseFloat(Config.Wrap("AccuratePings", "Angle", "Angle in degrees of the cone in which objects are pinged. (default: 10 | suggested: 3 | min: 1)", "3.0").Value, 3.0f, 1f);
+        public void Awake()
+        {
+            IL.RoR2.PingerController.AttemptPing += il =>
+            {
+                var c = new ILCursor(il);
+                c.GotoNext(x => x.MatchLdcR4(10));
+                c.Index++;
+                c.EmitDelegate<Func<float, float>>((f) => { return coneAngle; });
+            };
+        }
+
+    }
+}
---
+++ dist/README.md
@@ -0,0 +1,14 @@
+# AccuratePings
+
+Risk of Rain 2 mod to make pings more accurate
+
+ - When a player pings something, the game searches in a cone for the closest (?) object. This gets annoying when trying to ping things in the distanct with a closer object nearby.
+ - This mod makes the angle of the cone configurable, so you get a narrower / more accurate ping.
+
+
+Configuration is done in the `/BepInEx/config/com.Timmeh42.AccuratePings.cfg` file, which appears after loading the game once.
+
+### Installation
+
+- Requires BepInEx
+- Copy the dll to the BepinEx plugins folder
